c5250fb670e6e3c41d9b177deab01cf5421831f1
davidkurtz <info@go-faster.co.uk>
2018-04-06 15:18:05 +0100

    Enhance set_prcs_sess_parm
    Add KEYWORD parameter to permit other ALTER SESSION commands

--- set_prcs_sess_parm.sql
+++ set_prcs_sess_parm.sql
@@ -1,117 +1,135 @@
 REM set_prcs_sess_parm.sql
-REM see http://blog.psftdba.com/2018/03/setting-oracle-session-parameters-for.html
+REM 6.4.2018 added KEYWORD to permit other ALTER SESSION commands
 spool set_prcs_sess_parm
 rollback;
 alter session set current_schema=SYSADM;
-DROP TABLE sysadm.PS_PRCS_SESS_PARM 
-/
-CREATE TABLE sysadm.PS_PRCS_SESS_PARM (PRCSTYPE VARCHAR2(30) NOT NULL,
-   PRCSNAME VARCHAR2(12) NOT NULL,
-   OPRID VARCHAR2(30) NOT NULL,
-   RUNCNTLID VARCHAR2(30) NOT NULL,
-   PARAM_NAME VARCHAR2(50) NOT NULL,
-   PARMVALUE VARCHAR2(128) NOT NULL) TABLESPACE PTTBL STORAGE (INITIAL
- 40000 NEXT 100000 MAXEXTENTS UNLIMITED PCTINCREASE 0) PCTFREE 10
- PCTUSED 80
-/
-CREATE UNIQUE  iNDEX sysadm.PS_PRCS_SESS_PARM ON sysadm.PS_PRCS_SESS_PARM (PRCSTYPE,
-   PRCSNAME,
-   OPRID,
-   RUNCNTLID,
-   PARAM_NAME) TABLESPACE PSINDEX STORAGE (INITIAL 40000 NEXT 100000
- MAXEXTENTS UNLIMITED PCTINCREASE 0) PCTFREE 10 PARALLEL NOLOGGING
-/
-ALTER INDEX sysadm.PS_PRCS_SESS_PARM NOPARALLEL LOGGING
+@@set_prcs_sess_parm_trg.sql
+
+
+spool set_prcs_sess_parm app
+rollback;
+delete from sysadm.PS_PRCS_SESS_PARM where prcstype like 'nVision%';
+
+--Tried in production 
+--INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, keyword, param_name, parmvalue)
+--VALUES ('Application Engine','PSQUERY',' ',' ', 'SET', '_optimizer_skip_scan_enabled','FALSE');
+commit;
+
+INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, keyword, param_name, parmvalue)
+with x as (
+            select '_optimizer_skip_scan_enabled' param_name,'FALSE' parmvalue from dual
+--union all select 'parallel_degree_limit' param_name, '4' parmvalue from dual 
+--union all select 'parallel_degree_policy' , 'auto' from dual 
+--union all select 'parallel_degree_level', '200' from dual 
+--union all select 'parallel_min_time_threshold', '1' from dual
+union all select 'query_rewrite_enabled', 'FORCE' from dual /*added test 16*/
+), y as (
+select  prcstype, prcsname
+from	ps_prcsdefn
+where   prcsname IN('NVSRUN','RPTBOOK')
+and	prcstype like 'nVision-Report%'
+)
+select y.prcstype, y.prcsname, ' ', ' ', 'SET', x.param_name, x.parmvalue
+from x,y
 /
 
-rollback;
-delete from sysadm.PS_PRCS_SESS_PARM;
-REM this metadata is only a suggestion, your mileage will vary
-INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, param_name, parmvalue)
-VALUES ('nVision-ReportBook','RPTBOOK',' ',' ', 'parallel_degree_policy','auto');
-INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, param_name, parmvalue)
-VALUES ('nVision-ReportBook','RPTBOOK',' ',' ', 'parallel_degree_limit','4');
-INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, param_name, parmvalue)
-VALUES ('nVision-ReportBook','RPTBOOK',' ',' ', 'parallel_degree_level','150');
-INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, param_name, parmvalue)
-VALUES ('nVision-ReportBook','RPTBOOK',' ',' ', 'parallel_min_time_threshold','1');
-INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, param_name, parmvalue)
-VALUES ('nVision-ReportBook','RPTBOOK',' ',' ', '_optimizer_skip_scan_enabled','FALSE');
+------------
+--PARALLEL 4
+------------
+INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, keyword, param_name, parmvalue)
+with x as ( /*automatic parallelism parameters*/
+          select 'parallel_degree_limit' param_name, '4' parmvalue from dual 
+union all select 'parallel_degree_policy' , 'auto' from dual 
+union all select 'parallel_degree_level', '200' from dual 
+union all select 'parallel_min_time_threshold', '1' from dual 
+), y as ( /*nVision reports*/
+select  prcstype, prcsname
+from	ps_prcsdefn
+where   prcsname IN('NVSRUN','RPTBOOK')
+and	prcstype like 'nVision-Report%'
+), z as ( /*long running reports - over 4 hours*/
+          select 'NVISION' oprid, 'NVS_RPTBOOK_1' runcntlid from dual
+union all select 'NVISION', 'NVS_RPTBOOK_2' from dual /*added test 16*/
+union all select 'NVISION', 'NVS_RPTBOOK_5' from dual
+union all select 'NVISION', 'NVS_RPTBOOK_6' from dual
+union all select 'NVISION', 'NVS_RPTBOOK_10' from dual
+union all select 'NVISION', 'NVS_RPTBOOK_15' from dual
+union all select 'NVISION', 'NVS_RPTBOOK_16' from dual
+union all select 'NVISION', 'NVS_RPTBOOK_17' from dual
+union all select 'NVISION', 'NVS_RPTBOOK_20' from dual /*ADDED TEST 16*/
+union all select 'NVISION', 'NVS_RPTBOOK_22' from dual
+union all select 'NVISION', 'NVS_RPTBOOK_23' from dual
+union all select 'NVISION', 'NVS_RPTBOOK_24' from dual /*ADDED TEST 16*/
+union all select 'NVISION', 'NVS_RPTBOOK_25' from dual /*added test 15*/
+union all select 'NVISION', 'NVS_RPTBOOK_27' from dual
+union all select 'NVISION', 'LTD_7501_Direct_CF_M1' from dual
+union all select 'CANVISION', 'DIVISION' from dual /*added test 15*/
+union all select 'INTRUNCNTL', 'NVS_RPTBK_IRP2' from dual
+union all select 'INTRUNCNTL', 'NVS_RPTBK_IRP4' from dual
+union all select 'INTRUNCNTL', 'NVS_RPTBK_LOBA1' from dual
+union all select 'INTRUNCNTL', 'NVS_RPTBK_LOBA6' from dual
+union all select 'INTRUNCNTL', 'NVS_RPTBK_MORYTD1' from dual
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_1' from dual
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_2' from dual
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_3' from dual /*added test 15*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_4' from dual /*added test 16*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_5' from dual /*added test 15*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_10' from dual /*added test 15*/
+)
+select y.prcstype, y.prcsname, z.oprid, z.runcntlid, 'SET', x.param_name, x.parmvalue
+from x,y,z
+/
+
+------------
+--PARALLEL 4 was 6
+------------
+INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, keyword, param_name, parmvalue)
+with x as ( /*automatic parallelism parameters*/
+          select 'parallel_degree_limit' param_name, '4' parmvalue from dual 
+union all select 'parallel_degree_policy' , 'auto' from dual 
+union all select 'parallel_degree_level', '200' from dual 
+union all select 'parallel_min_time_threshold', '1' from dual 
+), y as ( /*nVision reports*/
+select  prcstype, prcsname
+from	ps_prcsdefn
+where   prcsname IN('NVSRUN','RPTBOOK')
+and	prcstype like 'nVision-Report%'
+), z as ( /*long running reports - over 4 hours*/
+          select 'NVISION' oprid, 'NVS_RPTBOOK_4' runcntlid from dual /*parallel 6 from test 22*/
+union all select 'NVISION', 'NVS_RPTBOOK_7' from dual /*parallel 6 from test 22*/
+union all select 'NVISION', 'NVS_RPTBOOK_14' from dual /*parallel 6 from test 22*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_6' from dual /*parallel 6 from test 22*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_7' from dual /*ADDED test 17, parallel 6 from test 22*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO1' from dual /*added test 22, parallel 6 from test 22*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO2' from dual /*added test 15, parallel 6 from test 22*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO3' from dual /*added test 15, parallel 6 from test 22*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO4' from dual /*added test 15, parallel 6 from test 22*/
+union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO5' from dual /*added test 15, parallel 6 from test 22*/
+)
+select y.prcstype, y.prcsname, z.oprid, z.runcntlid, 'SET', x.param_name, x.parmvalue
+from x,y,z
+/
+
 commit;
 
 column prcstype   format a20
 column oprid      format a10
 column param_name format a30
-column runcntlid  format a15
+column runcntlid  format a24
 column parmvalue  format a20
-select * from sysadm.PS_PRCS_SESS_PARM;
-
-set serveroutput on
-
-rollback;
-CREATE OR REPLACE TRIGGER sysadm.set_prcs_sess_parm
-BEFORE UPDATE OF runstatus ON sysadm.psprcsrqst
-FOR EACH ROW
-WHEN (new.runstatus = 7 
-  AND old.runstatus != 7 
-  AND new.prcstype != 'PSJob')
-DECLARE
-  l_cmd VARCHAR2(100 CHAR);
-  l_delim VARCHAR2(1 CHAR);
-BEGIN
-  FOR i IN (
-    WITH x as (
-      SELECT p.*
-      ,      row_number() over (partition by param_name 
-                               order by NULLIF(prcstype, ' ') nulls last,
-                                        NULLIF(prcsname, ' ') nulls last,
-                                        NULLIF(oprid, ' ') nulls last,
-                                        NULLIF(runcntlid,' ') nulls last
-                              ) priority
-      FROM   PS_PRCS_SESS_PARM p
-      WHERE  (p.prcstype  = :new.prcstype  OR p.prcstype  = ' ')
-      AND    (p.prcsname  = :new.prcsname  OR p.prcsname  = ' ')
-      AND    (p.oprid     = :new.oprid     OR p.oprid     = ' ')
-      AND    (p.runcntlid = :new.runcntlid OR p.runcntlid = ' ')
-    ) 
-    SELECT * FROM x
-    WHERE priority = 1 
-  ) LOOP
-    IF SUBSTR(i.param_name,1,1) = '_' THEN 
-      l_delim := '"';
-    ELSE
-      l_delim := '';
-    END IF;
-
-    IF NULLIF(i.parmvalue,' ') IS NOT NULL THEN
-      dbms_output.put_line('Rule:'||NVL(NULLIF(i.prcstype,' '),'*')
-                             ||'.'||NVL(NULLIF(i.prcsname,' '),'*')
-                             ||':'||NVL(NULLIF(i.oprid,' '),'*')
-                             ||'.'||NVL(NULLIF(i.runcntlid,' '),'*')
-                             ||':'||i.param_name||'='||i.parmvalue);
-
-      l_cmd := 'ALTER SESSION SET '||l_delim||i.param_name||l_delim||'='||i.parmvalue;
-      dbms_output.put_line('PI='||:new.prcsinstance||':'||:new.prcstype||'.'||:new.prcsname||':'
-                                ||:new.oprid||'.'||:new.runcntlid||':'||l_cmd);
-      EXECUTE IMMEDIATE l_cmd;
-    END IF;
-  END LOOP;
-EXCEPTION
-  WHEN OTHERS THEN
-    dbms_output.put_line(sqlerrm);
-END;
-/
-show errors
+select * from sysadm.PS_PRCS_SESS_PARM order by 1,2,3,4,5;
 
 --column text format a129 word_wrapped on 
 --select line, text from user_source
 --where name = 'SET_PRCS_SESS_PARM';
 
 --drop TRIGGER sysadm.set_prcs_sess_parm;
-/*
+Alter TRIGGER sysadm.set_prcs_sess_parm enable;
+
+
 set serveroutput on 
 rollback;
-update psprcsrqst
+update sysadm.psprcsrqst
 set runstatus = 7
 where runstatus != 7
 and prcsname = 'RPTBOOK'
@@ -119,7 +137,7 @@ and runcntlid = 'NVS_RPTBOOK_2'
 and oprid = 'NVISION'
 and rownum = 1;
 rollback;
-update psprcsrqst
+update sysadm.psprcsrqst
 set runstatus = 7
 where runstatus != 7
 and prcsname = 'RPTBOOK'
@@ -127,6 +145,7 @@ and runcntlid = 'NVS_RPTBOOK_1'
 and oprid = 'NVISION'
 and rownum = 1;
 rollback;
-*/
+
+alter session set current_schema=SYSADM;
 spool off
 
---
+++ set_prcs_sess_parm_trg.sql
@@ -0,0 +1,95 @@
+REM set_prcs_sess_parm_trg.sql
+REM 6.4.2018 added KEYWORD to permit other ALTER SESSION commands
+set echo on
+spool set_prcs_sess_parm_trg
+
+rollback;
+alter session set current_schema=SYSADM;
+
+REM DROP TABLE sysadm.PS_PRCS_SESS_PARM;
+CREATE TABLE sysadm.PS_PRCS_SESS_PARM (PRCSTYPE VARCHAR2(30) NOT NULL,
+   PRCSNAME VARCHAR2(12) NOT NULL,
+   OPRID VARCHAR2(30) NOT NULL,
+   RUNCNTLID VARCHAR2(30) NOT NULL,
+   KEYWORD VARCHAR2(8) NOT NULL, /*keyword is uset to specify first word after ALTER SESSION command - SET, ENABLE, FORCE etc*/
+   PARAM_NAME VARCHAR2(50) NOT NULL,
+   PARMVALUE VARCHAR2(128) NOT NULL) TABLESPACE PTTBL STORAGE (INITIAL
+ 40000 NEXT 100000 MAXEXTENTS UNLIMITED PCTINCREASE 0) PCTFREE 10
+ PCTUSED 80
+/
+CREATE UNIQUE  iNDEX sysadm.PS_PRCS_SESS_PARM ON sysadm.PS_PRCS_SESS_PARM (PRCSTYPE,
+   PRCSNAME,
+   OPRID,
+   RUNCNTLID,
+   PARAM_NAME) TABLESPACE PSINDEX STORAGE (INITIAL 40000 NEXT 100000
+ MAXEXTENTS UNLIMITED PCTINCREASE 0) PCTFREE 10 PARALLEL NOLOGGING
+/
+ALTER INDEX sysadm.PS_PRCS_SESS_PARM NOPARALLEL LOGGING
+/
+
+set serveroutput on
+
+rollback;
+CREATE OR REPLACE TRIGGER sysadm.set_prcs_sess_parm
+BEFORE UPDATE OF runstatus ON sysadm.psprcsrqst
+FOR EACH ROW
+WHEN (new.runstatus = 7 
+  AND old.runstatus != 7 
+  AND new.prcstype != 'PSJob')
+DECLARE
+  l_cmd VARCHAR2(100 CHAR);
+  l_delim VARCHAR2(1 CHAR) := '';
+  l_op VARCHAR2(1 CHAR) := '=';
+BEGIN
+  dbms_output.put_line('Row:'||:new.prcstype||'.'||:new.prcsname||':'||:new.oprid||'.'||:new.runcntlid);
+
+  FOR i IN (
+    WITH x as (
+      SELECT p.*
+      ,      row_number() over (partition by param_name 
+                               order by NULLIF(prcstype, ' ') nulls last,
+                                        NULLIF(prcsname, ' ') nulls last,
+                                        NULLIF(oprid, ' ') nulls last,
+                                        NULLIF(runcntlid,' ') nulls last
+                              ) priority
+      FROM   sysadm.PS_PRCS_SESS_PARM p
+      WHERE  (p.prcstype  = :new.prcstype  OR p.prcstype  = ' ')
+      AND    (p.prcsname  = :new.prcsname  OR p.prcsname  = ' ')
+      AND    (p.oprid     = :new.oprid     OR p.oprid     = ' ')
+      AND    (p.runcntlid = :new.runcntlid OR p.runcntlid = ' ')
+    ) 
+    SELECT * FROM x
+    WHERE priority = 1 
+  ) LOOP
+
+    IF UPPER(i.keyword) = 'SET' THEN
+      l_op := '=';
+      IF SUBSTR(i.param_name,1,1) = '_' THEN 
+        l_delim := '"';
+      ELSE
+        l_delim := '';
+      END IF;   
+    ELSE 
+      l_op := ' ';
+      l_delim := '';
+    END IF;
+
+    IF NULLIF(i.parmvalue,' ') IS NOT NULL THEN
+      dbms_output.put_line('Rule:'||NVL(NULLIF(i.prcstype,' '),'*')
+                             ||'.'||NVL(NULLIF(i.prcsname,' '),'*')
+                             ||':'||NVL(NULLIF(i.oprid,' '),'*')
+                             ||'.'||NVL(NULLIF(i.runcntlid,' '),'*')
+                             ||':'||i.keyword||':'||i.param_name||l_op||i.parmvalue);
+
+      l_cmd := 'ALTER SESSION '||i.keyword||' '||l_delim||i.param_name||l_delim||l_op||i.parmvalue;
+      dbms_output.put_line('PI='||:new.prcsinstance||':'||:new.prcstype||'.'||:new.prcsname||':'
+                                ||:new.oprid||'.'||:new.runcntlid||':'||l_cmd);
+      EXECUTE IMMEDIATE l_cmd;
+    END IF;
+  END LOOP;
+EXCEPTION
+  WHEN OTHERS THEN
+    dbms_output.put_line(sqlerrm);
+END;
+/
+show errors
