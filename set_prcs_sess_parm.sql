a3570b307b844db067f48f88ecd06e1f1f407d13
davidkurtz <info@go-faster.co.uk>
2018-04-06 15:20:26 +0100

    Tidy up

--- set_prcs_sess_parm.sql
+++ set_prcs_sess_parm.sql
@@ -10,19 +10,19 @@ spool set_prcs_sess_parm app
 rollback;
 delete from sysadm.PS_PRCS_SESS_PARM where prcstype like 'nVision%';
 
---Tried in production 
+--Tried in production, not successful
 --INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, keyword, param_name, parmvalue)
 --VALUES ('Application Engine','PSQUERY',' ',' ', 'SET', '_optimizer_skip_scan_enabled','FALSE');
 commit;
 
 INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, keyword, param_name, parmvalue)
 with x as (
-            select '_optimizer_skip_scan_enabled' param_name,'FALSE' parmvalue from dual
---union all select 'parallel_degree_limit' param_name, '4' parmvalue from dual 
---union all select 'parallel_degree_policy' , 'auto' from dual 
---union all select 'parallel_degree_level', '200' from dual 
---union all select 'parallel_min_time_threshold', '1' from dual
-union all select 'query_rewrite_enabled', 'FORCE' from dual /*added test 16*/
+          select '_optimizer_skip_scan_enabled' param_name,'FALSE' parmvalue from dual
+union all select 'parallel_degree_limit' param_name, '4' parmvalue from dual 
+union all select 'parallel_degree_policy' , 'auto' from dual 
+union all select 'parallel_degree_level', '200' from dual 
+union all select 'parallel_min_time_threshold', '1' from dual
+union all select 'query_rewrite_enabled', 'FORCE' from dual 
 ), y as (
 select  prcstype, prcsname
 from	ps_prcsdefn
@@ -31,83 +31,6 @@ and	prcstype like 'nVision-Report%'
 )
 select y.prcstype, y.prcsname, ' ', ' ', 'SET', x.param_name, x.parmvalue
 from x,y
-/
-
-------------
---PARALLEL 4
-------------
-INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, keyword, param_name, parmvalue)
-with x as ( /*automatic parallelism parameters*/
-          select 'parallel_degree_limit' param_name, '4' parmvalue from dual 
-union all select 'parallel_degree_policy' , 'auto' from dual 
-union all select 'parallel_degree_level', '200' from dual 
-union all select 'parallel_min_time_threshold', '1' from dual 
-), y as ( /*nVision reports*/
-select  prcstype, prcsname
-from	ps_prcsdefn
-where   prcsname IN('NVSRUN','RPTBOOK')
-and	prcstype like 'nVision-Report%'
-), z as ( /*long running reports - over 4 hours*/
-          select 'NVISION' oprid, 'NVS_RPTBOOK_1' runcntlid from dual
-union all select 'NVISION', 'NVS_RPTBOOK_2' from dual /*added test 16*/
-union all select 'NVISION', 'NVS_RPTBOOK_5' from dual
-union all select 'NVISION', 'NVS_RPTBOOK_6' from dual
-union all select 'NVISION', 'NVS_RPTBOOK_10' from dual
-union all select 'NVISION', 'NVS_RPTBOOK_15' from dual
-union all select 'NVISION', 'NVS_RPTBOOK_16' from dual
-union all select 'NVISION', 'NVS_RPTBOOK_17' from dual
-union all select 'NVISION', 'NVS_RPTBOOK_20' from dual /*ADDED TEST 16*/
-union all select 'NVISION', 'NVS_RPTBOOK_22' from dual
-union all select 'NVISION', 'NVS_RPTBOOK_23' from dual
-union all select 'NVISION', 'NVS_RPTBOOK_24' from dual /*ADDED TEST 16*/
-union all select 'NVISION', 'NVS_RPTBOOK_25' from dual /*added test 15*/
-union all select 'NVISION', 'NVS_RPTBOOK_27' from dual
-union all select 'NVISION', 'LTD_7501_Direct_CF_M1' from dual
-union all select 'CANVISION', 'DIVISION' from dual /*added test 15*/
-union all select 'INTRUNCNTL', 'NVS_RPTBK_IRP2' from dual
-union all select 'INTRUNCNTL', 'NVS_RPTBK_IRP4' from dual
-union all select 'INTRUNCNTL', 'NVS_RPTBK_LOBA1' from dual
-union all select 'INTRUNCNTL', 'NVS_RPTBK_LOBA6' from dual
-union all select 'INTRUNCNTL', 'NVS_RPTBK_MORYTD1' from dual
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_1' from dual
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_2' from dual
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_3' from dual /*added test 15*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_4' from dual /*added test 16*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_5' from dual /*added test 15*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_10' from dual /*added test 15*/
-)
-select y.prcstype, y.prcsname, z.oprid, z.runcntlid, 'SET', x.param_name, x.parmvalue
-from x,y,z
-/
-
-------------
---PARALLEL 4 was 6
-------------
-INSERT INTO sysadm.PS_PRCS_SESS_PARM (prcstype, prcsname, oprid, runcntlid, keyword, param_name, parmvalue)
-with x as ( /*automatic parallelism parameters*/
-          select 'parallel_degree_limit' param_name, '4' parmvalue from dual 
-union all select 'parallel_degree_policy' , 'auto' from dual 
-union all select 'parallel_degree_level', '200' from dual 
-union all select 'parallel_min_time_threshold', '1' from dual 
-), y as ( /*nVision reports*/
-select  prcstype, prcsname
-from	ps_prcsdefn
-where   prcsname IN('NVSRUN','RPTBOOK')
-and	prcstype like 'nVision-Report%'
-), z as ( /*long running reports - over 4 hours*/
-          select 'NVISION' oprid, 'NVS_RPTBOOK_4' runcntlid from dual /*parallel 6 from test 22*/
-union all select 'NVISION', 'NVS_RPTBOOK_7' from dual /*parallel 6 from test 22*/
-union all select 'NVISION', 'NVS_RPTBOOK_14' from dual /*parallel 6 from test 22*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_6' from dual /*parallel 6 from test 22*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_7' from dual /*ADDED test 17, parallel 6 from test 22*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO1' from dual /*added test 22, parallel 6 from test 22*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO2' from dual /*added test 15, parallel 6 from test 22*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO3' from dual /*added test 15, parallel 6 from test 22*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO4' from dual /*added test 15, parallel 6 from test 22*/
-union all select 'USRUNCNTRL', 'NVS_RPTBOOK_GEO5' from dual /*added test 15, parallel 6 from test 22*/
-)
-select y.prcstype, y.prcsname, z.oprid, z.runcntlid, 'SET', x.param_name, x.parmvalue
-from x,y,z
 /
 
 commit;
