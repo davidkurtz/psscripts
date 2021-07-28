5fdc55f19e9f554a38e4ee49c7edae2ab961b73f
David Kurtz <info@go-faster.co.uk>
2021-07-28 17:30:16 +0100

    Update calc_opt_comp.sql
    Calculate optimal index compression for a named table

--- calc_opt_comp.sql
+++ calc_opt_comp.sql
@@ -88,20 +88,34 @@ column index_name format a18
 column partition_name format a30
 column name format a18
 column freq format 999
-column parts format 999
+column parts heading 'Num|Parts' format 9999
 column prefix_length heading 'Index|Prefix|Length'
 column weighted_average_saving format 99.9 heading 'Weighted|Average|Saving %'
 column opt_cmpr_count heading 'Opt Comp|Prefix|Length'
-column opt_cmpr_pctsave format 99.9 heading 'Saving %'
+column opt_cmpr_pctsave format 99.9 heading 'Saving|%'
+column blocks heading 'Blocks' format 999,999,999
+column est_comp_blocks heading 'Est.|Comp|Blocks' format 999,999,999
+column tot_blocks heading 'Total|Blocks' format 999,999,999
+column tot_parts  heading 'Total|Parts'  format 999,999
 break on table_name skip 1 on name skip 1 on report 
+compute sum of blocks on name 
+compute sum of blocks on table_name 
+compute sum of blocks on report
+compute sum of est_comp_blocks on name 
+compute sum of est_comp_blocks on table_name 
+compute sum of est_comp_blocks on report
+compute sum of parts on name 
+compute sum of parts on table_name 
+compute sum of parts on report
 ttitle 'Summary Report'
-set lines 80 pages 99
+set lines 120 pages 99
 rem name skip 1
 SELECT i.table_name, s.name, s.opt_cmpr_count
 , count(*) freq
 , count(partition_name) parts
 , sum(s.blocks) blocks
 , sum(s.opt_cmpr_pctsave*blocks)/sum(s.blocks) weighted_average_saving
+, sum((1-s.opt_cmpr_pctsave/100)*blocks) est_comp_blocks
 FROM sysadm.gfc_index_stats s, dba_indexes i
 WHERE s.name = i.index_name
 AND i.owner = 'SYSADM'
@@ -110,53 +124,62 @@ GROUP BY i.table_name, s.name, s.opt_cmpr_count
 ORDER BY i.table_name, s.name, s.opt_cmpr_count
 /
 
+
 break on table_name on index_name skip 1
-set lines 150
+compute sum of blocks on index_name
+compute sum of est_comp_blocks on index_name 
+compute count of partition_name on index_name
+compute count of partition_name on table_name
+set lines 170
 ttitle 'Partitions with Lower Optimal Prefix Length than Majority'
 WITH s AS (
 select 	i.table_name, i.index_name, i.prefix_length, s.opt_cmpr_count
-,	s.partition_name
-,	s.blocks
-,	s.opt_cmpr_pctsave
+, 	    s.partition_name
+,	    s.blocks
+,	    s.opt_cmpr_pctsave
 from	sysadm.gfc_index_stats s, dba_indexes i
 WHERE 	s.name = i.index_name
 AND 	i.owner = 'SYSADM'
 ), x as (
 SELECT  table_name, index_name, opt_cmpr_count
-, 	count(*) freq
-, 	count(partition_name) parts
-, 	sum(blocks) blocks
-, 	sum(opt_cmpr_pctsave*blocks)/sum(blocks) weighted_average_saving
+, 	    count(*) freq
+, 	    count(partition_name) parts
+, 	    sum(blocks) blocks
+, 	    sum(opt_cmpr_pctsave*blocks)/sum(blocks) weighted_average_saving
 FROM 	s
 --AND 	blocks > 256
 GROUP BY table_name, index_name, opt_cmpr_count
 ), y as (
-select 	 row_number() over (partition by table_name, index_name order by blocks desc) ranking
-,	 x.*
-from	 x
+select row_number() over (partition by table_name, index_name order by blocks desc) ranking
+,      x.*
+from   x
 )
-select 	s.table_name, s.index_name, s.prefix_length
-, 	y.opt_cmpr_count, y.parts, y.blocks
-,	s.partition_name
-, 	s.opt_cmpr_count, s.blocks, s.opt_cmpr_pctsave
-from	y
-,	s
-where	y.table_name = s.table_name
-and	y.index_name = s.index_name
-and	s.opt_cmpr_count < y.opt_cmpr_count
-and	y.ranking = 1
+select s.table_name, s.index_name, s.prefix_length
+, 	   y.opt_cmpr_count, y.parts, y.blocks
+,	   s.partition_name
+, 	   s.opt_cmpr_count, s.blocks, s.opt_cmpr_pctsave
+,      ((1-s.opt_cmpr_pctsave/100)*s.blocks) est_comp_blocks
+from   y
+,	   s
+where  y.table_name = s.table_name
+and	   y.index_name = s.index_name
+and	   s.opt_cmpr_count < y.opt_cmpr_count
+and	   y.ranking = 1
 order by table_name, index_name, partition_name
 /
 
-set lines 110
+set lines 130
 ttitle 'Detail Report'
+break on table_name on name skip 1
 SELECT i.table_name, s.name, s.partition_name, s.opt_cmpr_count
-, s.blocks
-, s.opt_cmpr_pctsave
-FROM sysadm.gfc_index_stats s, dba_indexes i
-WHERE s.name = i.index_name
-AND i.owner = 'SYSADM'
+,      s.blocks
+,      s.opt_cmpr_pctsave
+,      ((1-s.opt_cmpr_pctsave/100)*s.blocks) est_comp_blocks
+FROM   sysadm.gfc_index_stats s, dba_indexes i
+WHERE  s.name = i.index_name
+AND    i.owner = 'SYSADM'
 ORDER BY i.table_name, s.name, s.partition_name, s.opt_cmpr_count
+FETCH FIRST 50 ROWS ONLY
 /
 
 spool off
