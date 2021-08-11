e05a960441bbdd1c5797c5df6cb6763c2a756c29
David Kurtz <info@go-faster.co.uk>
2021-08-11 14:48:43 +0100

    Update depend_hier.sql

--- depend_hier.sql
+++ depend_hier.sql
@@ -6,23 +6,21 @@ ttitle 'Dependency Hierarchy'
 column my_level format a5 heading 'Level'
 column owner format a12
 column name format a18
+column type format a7
 column referenced_type format a7 heading 'Refd|Type'
-column referenced_owner format a5 heading 'Refd|Owner'
+column referenced_owner format a6 heading 'Refd|Owner'
 column referenced_name format a18 heading 'Refd|Name'
 column referenced_link_name format a10 heading 'Refd|Link'
-column type format a7
-column owner format a5
-column name format a18
 column dependency_type heading 'Dep|Type'
-column text heading 'View Text' format a90 wrap on
+column text heading 'View Text' format a80 wrap on
 spool depend_hier.&&object_name..lst
 select LPAD(TO_CHAR(level),level,'.') my_level
 , d.type, d.owner, d.name
 , d.referenced_type, d.referenced_owner, d.referenced_name, d.referenced_link_name
 , d.dependency_type
 , v.text
-from dba_dependencies d
-  left outer join dba_views v 
+from all_dependencies d
+  left outer join all_views v 
     on  v.owner = d.referenced_owner
     and v.view_name = d.referenced_name
   connect by nocycle
