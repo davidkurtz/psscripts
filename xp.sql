20496aeb1cc031c293e75715b2238bcc8d94e70b
David Kurtz <info@go-faster.co.uk>
2022-03-08 17:19:10 +0000

    Update xp.sql
    Correction

--- xp.sql
+++ xp.sql
@@ -1,6 +1,6 @@
 spool xp
 set pages 9999 lines 200 autotrace off
-select * from table(dbms_xplan.display(null,null,'ADVANCED +ADAPTIVE -PROJECTION +ADPATIVE'))
+select * from table(dbms_xplan.display(null,null,'ADVANCED +ADAPTIVE -PROJECTION'))
 /
 spool off
 
